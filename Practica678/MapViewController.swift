824012b76aec7585d2f40823c315ff8eef0bd782
XCodeClub <xcodeclub@xcodeclubs-mac-60.local>
2018-10-10 13:28:43 -0400

    final

--- Practica678/MapViewController.swift
+++ Practica678/MapViewController.swift
@@ -39,34 +39,11 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         map.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         let cordenada = CLLocationCoordinate2D(latitude: lat!, longitude: lng!)
         let anotacion = Anotacion(cordinate: cordenada, title: name!)
-        let cordenadaO = CLLocationCoordinate2D(latitude: 20.744478, longitude: -103.378901)
-        let anotacionO = Anotacion(cordinate: cordenadaO, title: "Point")
+       
         
         map.addAnnotation(anotacion)
-        map.addAnnotation(anotacionO)
         
-        let inicioLugar = MKPlacemark(coordinate: cordenadaO)
-        let finLugar = MKPlacemark(coordinate: cordenada)
-        let directionRequest = MKDirectionsRequest()
-        
-        directionRequest.source = MKMapItem(placemark: inicioLugar)
-        directionRequest.destination = MKMapItem(placemark: finLugar)
-        directionRequest.transportType = .automobile
-        
-        let direction = MKDirections (request: directionRequest)
-        direction.calculate {(response, error) in
-            guard let directionsResonse = response else{
-                if error != nil{
-                    print("error")
-                }
-                return
-            }
-            let route = directionsResonse.routes[0]
-            self.map.add(route.polyline, level: .aboveRoads)
-            let ract = route.polyline.boundingMapRect
-            self.map.setRegion(MKCoordinateRegionForMapRect(ract), animated: true)
-        }
-        self.map.delegate = self
+       
         //map.setRegion(anotacion.region, animated: true)
     }
     
